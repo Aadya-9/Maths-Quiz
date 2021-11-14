bdc23b012f4b0386ab06c67b2fddcbd6f0bfbbb5
Aadya-9 <82094543+Aadya-9@users.noreply.github.com>
2021-11-14 07:14:14 +0530

    Add files via upload

--- game_page.js
+++ game_page.js
@@ -22,3 +22,38 @@ function send(){
     
     
 }
+question_turn="player1";
+answer_turn="player2";
+
+function check(){
+answer= document.getElementById("input_check_box").value ;
+correct_answer= number1*number2;
+if(answer==correct_answer){
+   
+    if(answer_turn=="player1"){
+        player1_score=player1_score+1;
+    document.getElementById("player1_score").innerHTML=player1_score;
+    }
+    else{
+        player2_score=player2_score+1;
+        document.getElementById("player2_score").innerHTML=player2_score;
+    }
+}
+if(question_turn=="player1"){
+    question_turn="player2";
+    document.getElementById("player_question").innerHTML="question turn- "+player2_name;
+}
+else{
+    question_turn="player1";
+    document.getElementById("player_question").innerHTML="question turn- "+player1_name;
+}
+if(answer_turn=="player1"){
+    answer_turn="player2";
+    document.getElementById("player_answer").innerHTML="answer turn- "+player2_name;
+}
+else{
+    answer_turn="player1";
+    document.getElementById("player_answer").innerHTML="answer turn- "+player1_name;
+}
+document.getElementById("output").innerHTML="";
+}
